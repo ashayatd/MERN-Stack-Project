9705604a2efb3121b68d0af7e11f799405b79c83
Ashay Tamrakar <48744162+ashayatd@users.noreply.github.com>
2023-02-23 18:17:51 +0530

    Frontend loin CSS

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { useState} from "react";
+import { useState } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import "./Login.css";
 
@@ -31,75 +31,81 @@ const Login = () => {
 
     if (res.status !== 200) {
       window.alert(data.message);
-    } 
-    else {
+    } else {
       console.log(data.message);
-      if(data.role==="user"){
+      if (data.role === "user") {
         navigate("/dashboard");
       }
-      if(data.role==="admin"){
-      navigate("/admindashboard");
+      if (data.role === "admin") {
+        navigate("/admindashboard");
       }
     }
   };
 
   return (
-    <>
-    <div>
-    <nav id="navbar">
-      <ul>
-        <li><Link Link to="/about" className="Link">
-        About
-      </Link></li>
-        <li><Link Link to="/register" className="Link">
-        Register
-      </Link></li>
-      </ul>
-    </nav>
-    <Outlet />
-  </div>
-
-    <div>
+    <div className="main_background">
+      <div className="navbarBackground">
+        <div className="navbarBackground2"></div>
+      </div>
       <div>
-         {/*  */}
-        <div className="login-container">
-          <form className="login-form">
-            <h1 className="form-title">Login</h1>
-            <div className="form-group">
-              <label htmlFor="username">Username:</label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                value={Username}
-                onChange={(event) => setUsername(event.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={Password}
-                onChange={(event) => setPassword(event.target.value)}
-                required
-              />
-            </div>
-            <button type="submit" onClick={loginUser}>
-              Login
-            </button>
-          </form>
+        <nav>
+          <ul id="navbar">
+            <li>
+              <Link Link to="/about" className="Link">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link Link to="/register" className="Link">
+                Register
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </div>
+
+      <div>
+        <div className="main-div">
+          <div className="login-container">
+                <form className="login-form">
+                  <h1 className="form-title">Login</h1>
+                  <div className="form-group">
+                    <label htmlFor="username" className="label">Username:</label>
+                    <input
+                      type="text"
+                      id="username"
+                      name="username"
+                      value={Username}
+                      onChange={(event) => setUsername(event.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="password" className="label">Password:</label>
+                    <input 
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={Password}
+                      onChange={(event) => setPassword(event.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="button-div">
+                    <button type="submit" onClick={loginUser} className="button">
+                      Login
+                    </button>
+                  </div>
+                  
+                </form>
+              <img src="https://productsinsights.com/wp-content/uploads/2023/02/Untitled-design-20.png" className="side-image"/>
+              
+                
+          </div>
         </div>
       </div>
     </div>
-    </>
   );
 };
 export default Login;
-
-
-
-
-
